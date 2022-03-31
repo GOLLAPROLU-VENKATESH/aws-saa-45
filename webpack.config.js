bfe0eeec22349732f26600918ee55bca2f8855f3
GOLLAPROLU-VENKATESH <gollaprolu.venkatesh1501@gmail.com>
2022-03-31 07:10:35 +0530

    comit

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,11 @@ module.exports = {
     },
     externals: {
         // global app config object
+        // config: JSON.stringify({
+        //     apiUrl: 'http://localhost:4000'
+        // })
         config: JSON.stringify({
-            apiUrl: 'http://localhost:4000'
+            apiUrl: '/api'
         })
     }
 }
