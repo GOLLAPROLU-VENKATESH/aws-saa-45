963c72c02ab123c4460db44ab9e53c443200f9c1
GOLLAPROLU-VENKATESH <gollaprolu.venkatesh1501@gmail.com>
2022-03-31 06:50:45 +0530

    Update App.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -24,7 +24,7 @@ class App extends React.Component {
         return (
             
                 <div className="container">
-                   <center><h3>AWS SAA - 45</h3></center> 
+                    <center><h3>AWS SAA - 45</h3></center> 
                     <div className="col-sm-8 col-sm-offset-2">
                         {alert.message &&
                             <div className={`alert ${alert.type}`}>{alert.message}</div>
